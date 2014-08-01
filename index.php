32dbc1ee9e2e162d59847bbeb154131f963b2a33
daniel spillere andrade <daniel@danielandrade.net>
2014-08-01 12:34:59 -0300

    Fix Lightbox

--- index.php
+++ index.php
@@ -27,28 +27,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>daniel andrade - photos</title>
 
-	<link rel='stylesheet' href='css/colorbox.css?ver=4.6' type='text/css' media='screen' />
+	<link href="css/lightbox.css" rel="stylesheet" />
+	
 	<link rel='stylesheet' href='css/justifiedGallery.min.css?ver=3.9.1' type='text/css' media='all' />
 	<link rel='stylesheet' href='css/swipebox.min.css?ver=3.9.1' type='text/css' media='all' />
 	<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' type='text/css' media='all' />
 	<link rel='stylesheet' href='css/style.css' type='text/css' />
+	<link rel='stylesheet' href='css/lightbox.css' type='text/css' media='all' />
 
-	<?php if (isset($g)) { ?>
-		<script type='text/javascript'>
-		/* <![CDATA[ */
-			var jQueryColorboxSettingsArray = {"jQueryColorboxVersion":"4.6","colorboxInline":"false","colorboxIframe":"false","colorboxGroupId":"","colorboxTitle":"","colorboxWidth":"false","colorboxHeight":"false","colorboxMaxWidth":"false","colorboxMaxHeight":"false","colorboxSlideshow":"false","colorboxSlideshowAuto":"false","colorboxScalePhotos":"true","colorboxPreloading":"true","colorboxOverlayClose":"true","colorboxLoop":"true","colorboxEscKey":"true","colorboxArrowKey":"false","colorboxScrolling":"true","colorboxOpacity":"0.85","colorboxTransition":"elastic","colorboxSpeed":"350","colorboxSlideshowSpeed":"2500","colorboxClose":"close","colorboxNext":"next","colorboxPrevious":"previous","colorboxSlideshowStart":"start slideshow","colorboxSlideshowStop":"stop slideshow","colorboxCurrent":"{current} of {total} images","colorboxXhrError":"This content failed to load.","colorboxImgError":"This image failed to load.","colorboxImageMaxWidth":"100%","colorboxImageMaxHeight":"100%","colorboxImageHeight":"false","colorboxImageWidth":"false","colorboxLinkHeight":"false","colorboxLinkWidth":"false","colorboxInitialHeight":"100","colorboxInitialWidth":"300","autoColorboxJavaScript":"","autoHideFlash":"","autoColorbox":"true","autoColorboxGalleries":"","addZoomOverlay":"","useGoogleJQuery":"","colorboxAddClassToLinks":""};
-		/* ]]> */
-		</script>
-	<?php } ?>
 	<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-	<script type='text/javascript' src='jjs/query-migrate.min.js?ver=1.2.1'></script>
-	<script type='text/javascript' src='jjs/query-migrate.min.js?ver=1.2.1'></script>
-	<script type='text/javascript' src='js/jquery.colorbox-min.js?ver=1.3.21'></script>
-	<script type='text/javascript' src='js/jquery-colorbox-wrapper-min.js?ver=4.6'></script>
+	<script type='text/javascript' src='js/query-migrate.min.js?ver=1.2.1'></script>
 	<script type='text/javascript' src='js/jquery.justifiedGallery.min.js?ver=3.9.1'></script>
 	<script type='text/javascript' src='js/jquery.swipebox.min.js?ver=3.9.1'></script>
 	<script type='text/javascript' src='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
-
+	<script type='text/javascript' src='js/lightbox.min.js'></script>
 
   <script src="js/lazy.js"></script>
   <script type="text/javascript" charset="utf-8">
@@ -80,11 +72,12 @@
 		  $server = $pic['server'];
 		  $farm = $pic['farm']; 
 		  $title = $pic['title'];
-		  echo '<a href="https://farm'.$farm.'.staticflickr.com/'.$server.'/'.$id.'_'.$secret.'_b.jpg" onclick="javascript:_gaq.push([\'_trackEvent\',\'outbound-article\',\'http://farm'.$farm.'.staticflickr.com\']);" rel="flickrGal0" title="'.$title.'"><img class="colorbox-670"  alt="'.$title.'" src="https://farm'.$farm.'.staticflickr.com/'.$server.'/'.$id.'_'.$secret.'_n.jpg" data-safe-src="https://farm'.$farm.'.staticflickr.com/'.$server.'/'.$id.'_'.$secret.'_n.jpg" /></a>';
+		  echo '<a data-lightbox="image-1" href="https://farm'.$farm.'.staticflickr.com/'.$server.'/'.$id.'_'.$secret.'_b.jpg" rel="flickrGal0" title="'.$title.'"><img alt="'.$title.'" src="https://farm'.$farm.'.staticflickr.com/'.$server.'/'.$id.'_'.$secret.'_n.jpg" data-safe-src="https://farm'.$farm.'.staticflickr.com/'.$server.'/'.$id.'_'.$secret.'_n.jpg" /></a>';
 		endforeach;
 
 		echo "</div>";
 		?>
+
 	<script type="text/javascript">
 		jQuery("#flickrGal0").on('jg.rowflush', function() {jQuery(this).find("> a").colorbox({maxWidth : "100%",maxHeight : "100%",current : ""});}).justifiedGallery({'lastRow': 'justify', 'rowHeight':250, 'fixedHeight':false, 'captions':true, 'randomize':false, 'margins':10});
 		(function(){var e=document.createElement("link");var t=document.createElement("link");var n="css/shCore.css?ver=3.0.9";if(e.setAttribute){e.setAttribute("rel","stylesheet");e.setAttribute("type","text/css");e.setAttribute("href",n)}else{e.rel="stylesheet";e.href=n}document.getElementsByTagName("head")[0].insertBefore(e,document.getElementById("syntaxhighlighteranchor"));var r="css/shThemeDefault.css?ver=3.0.9";if(t.setAttribute){t.setAttribute("rel","stylesheet");t.setAttribute("type","text/css");t.setAttribute("href",r)}else{t.rel="stylesheet";t.href=r}document.getElementsByTagName("head")[0].insertBefore(t,document.getElementById("syntaxhighlighteranchor"))})();SyntaxHighlighter.config.strings.expandSource="+ expand source";SyntaxHighlighter.config.strings.help="?";SyntaxHighlighter.config.strings.alert="SyntaxHighlighter\n\n";SyntaxHighlighter.config.strings.noBrush="Can't find brush for: ";SyntaxHighlighter.config.strings.brushNotHtmlScript="Brush wasn't configured for html-script option: ";SyntaxHighlighter.defaults["class-name"]="code";SyntaxHighlighter.defaults["gutter"]=false;SyntaxHighlighter.defaults["pad-line-numbers"]=5;SyntaxHighlighter.defaults["toolbar"]=false;SyntaxHighlighter.all()
