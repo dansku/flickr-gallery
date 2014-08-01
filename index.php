c386a608d19d63f50f9605e54911935cf81d3893
daniel spillere andrade <daniel@danielandrade.net>
2014-08-01 12:17:44 -0300

    Fixed front page + gallery + header
    Signed-off-by: daniel spillere andrade <daniel@danielandrade.net>

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	
-	/* 	Flickr Gallery Script by Daniel Spillere Andrade - www.danielandrade.net
+	/* 	
+		Flickr Gallery Script by Daniel Spillere Andrade - www.danielandrade.net
 		based on
 	*/
 	
@@ -12,6 +13,7 @@
 		$data = simplexml_load_file('https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key='.$key.'&user_id='.$id.'&photoset_id='.$g);
 	}
 	else{
+		// Get a list of photosets
 		$album = simplexml_load_file('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key='.$key.'&user_id='.$id);	
 	}
 
@@ -23,26 +25,48 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Photo Gallery</title>
-	<link rel='stylesheet' id='colorbox-theme3-css'  href='css/colorbox.css?ver=4.6' type='text/css' media='screen' />
-	<link rel='stylesheet' id='justifiedGalleryCSS-css'  href='css/justifiedGallery.min.css?ver=3.9.1' type='text/css' media='all' />
-	<link rel='stylesheet' id='swipeboxCSS-css'  href='css/swipebox.min.css?ver=3.9.1' type='text/css' media='all' />
-	<link rel='stylesheet' id='bootstrap-css'  href='css/bootstrap.min.css?ver=1.0' type='text/css' media='all' />
-	<script type='text/javascript' src='js/jquery.js?ver=1.11.0'></script>
+	<title>daniel andrade - photos</title>
+
+	<link rel='stylesheet' href='css/colorbox.css?ver=4.6' type='text/css' media='screen' />
+	<link rel='stylesheet' href='css/justifiedGallery.min.css?ver=3.9.1' type='text/css' media='all' />
+	<link rel='stylesheet' href='css/swipebox.min.css?ver=3.9.1' type='text/css' media='all' />
+	<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' type='text/css' media='all' />
+	<link rel='stylesheet' href='css/style.css' type='text/css' />
+
+	<?php if (isset($g)) { ?>
+		<script type='text/javascript'>
+		/* <![CDATA[ */
+			var jQueryColorboxSettingsArray = {"jQueryColorboxVersion":"4.6","colorboxInline":"false","colorboxIframe":"false","colorboxGroupId":"","colorboxTitle":"","colorboxWidth":"false","colorboxHeight":"false","colorboxMaxWidth":"false","colorboxMaxHeight":"false","colorboxSlideshow":"false","colorboxSlideshowAuto":"false","colorboxScalePhotos":"true","colorboxPreloading":"true","colorboxOverlayClose":"true","colorboxLoop":"true","colorboxEscKey":"true","colorboxArrowKey":"false","colorboxScrolling":"true","colorboxOpacity":"0.85","colorboxTransition":"elastic","colorboxSpeed":"350","colorboxSlideshowSpeed":"2500","colorboxClose":"close","colorboxNext":"next","colorboxPrevious":"previous","colorboxSlideshowStart":"start slideshow","colorboxSlideshowStop":"stop slideshow","colorboxCurrent":"{current} of {total} images","colorboxXhrError":"This content failed to load.","colorboxImgError":"This image failed to load.","colorboxImageMaxWidth":"100%","colorboxImageMaxHeight":"100%","colorboxImageHeight":"false","colorboxImageWidth":"false","colorboxLinkHeight":"false","colorboxLinkWidth":"false","colorboxInitialHeight":"100","colorboxInitialWidth":"300","autoColorboxJavaScript":"","autoHideFlash":"","autoColorbox":"true","autoColorboxGalleries":"","addZoomOverlay":"","useGoogleJQuery":"","colorboxAddClassToLinks":""};
+		/* ]]> */
+		</script>
+	<?php } ?>
+	<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 	<script type='text/javascript' src='jjs/query-migrate.min.js?ver=1.2.1'></script>
-	<script type='text/javascript'>
-	/* <![CDATA[ */
-		var jQueryColorboxSettingsArray = {"jQueryColorboxVersion":"4.6","colorboxInline":"false","colorboxIframe":"false","colorboxGroupId":"","colorboxTitle":"","colorboxWidth":"false","colorboxHeight":"false","colorboxMaxWidth":"false","colorboxMaxHeight":"false","colorboxSlideshow":"false","colorboxSlideshowAuto":"false","colorboxScalePhotos":"true","colorboxPreloading":"true","colorboxOverlayClose":"true","colorboxLoop":"true","colorboxEscKey":"true","colorboxArrowKey":"false","colorboxScrolling":"true","colorboxOpacity":"0.85","colorboxTransition":"elastic","colorboxSpeed":"350","colorboxSlideshowSpeed":"2500","colorboxClose":"close","colorboxNext":"next","colorboxPrevious":"previous","colorboxSlideshowStart":"start slideshow","colorboxSlideshowStop":"stop slideshow","colorboxCurrent":"{current} of {total} images","colorboxXhrError":"This content failed to load.","colorboxImgError":"This image failed to load.","colorboxImageMaxWidth":"100%","colorboxImageMaxHeight":"100%","colorboxImageHeight":"false","colorboxImageWidth":"false","colorboxLinkHeight":"false","colorboxLinkWidth":"false","colorboxInitialHeight":"100","colorboxInitialWidth":"300","autoColorboxJavaScript":"","autoHideFlash":"","autoColorbox":"true","autoColorboxGalleries":"","addZoomOverlay":"","useGoogleJQuery":"","colorboxAddClassToLinks":""};
-	/* ]]> */
-	</script>
+	<script type='text/javascript' src='jjs/query-migrate.min.js?ver=1.2.1'></script>
 	<script type='text/javascript' src='js/jquery.colorbox-min.js?ver=1.3.21'></script>
 	<script type='text/javascript' src='js/jquery-colorbox-wrapper-min.js?ver=4.6'></script>
 	<script type='text/javascript' src='js/jquery.justifiedGallery.min.js?ver=3.9.1'></script>
 	<script type='text/javascript' src='js/jquery.swipebox.min.js?ver=3.9.1'></script>
-	<script type='text/javascript' src='js/bootstrap.min.js?ver=1.2'></script>
+	<script type='text/javascript' src='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
+
+
+  <script src="js/lazy.js"></script>
+  <script type="text/javascript" charset="utf-8">
+  $(function() {
+     $("img.lazy").lazyload({
+         effect : "fadeIn"
+     });
+  });
+  </script>
 </head>
 
-<body class="page page-id-670 page-child parent-pageid-240 page-template-default">
+<body class="page page-id-670 page-child parent-pageid-240 page-template-default" style="background-color: rgb(51, 51, 51);">
+
+<div class="header">
+	<p class="alignleft"><span class="glyphicon glyphicon-user"></span> Daniel Andrade</p>
+	<?php if (isset($g)){ echo '<a href="?"><p class="alignright"><span class="glyphicon glyphicon-chevron-left"></span> back to album list</p></a>';} ?>
+	<div style="clear: both;"></div>
+</div>
 
 <?php 
 	//	if g is set we load the pictures, otherwise the album list...
@@ -65,25 +89,57 @@
 		jQuery("#flickrGal0").on('jg.rowflush', function() {jQuery(this).find("> a").colorbox({maxWidth : "100%",maxHeight : "100%",current : ""});}).justifiedGallery({'lastRow': 'justify', 'rowHeight':250, 'fixedHeight':false, 'captions':true, 'randomize':false, 'margins':10});
 		(function(){var e=document.createElement("link");var t=document.createElement("link");var n="css/shCore.css?ver=3.0.9";if(e.setAttribute){e.setAttribute("rel","stylesheet");e.setAttribute("type","text/css");e.setAttribute("href",n)}else{e.rel="stylesheet";e.href=n}document.getElementsByTagName("head")[0].insertBefore(e,document.getElementById("syntaxhighlighteranchor"));var r="css/shThemeDefault.css?ver=3.0.9";if(t.setAttribute){t.setAttribute("rel","stylesheet");t.setAttribute("type","text/css");t.setAttribute("href",r)}else{t.rel="stylesheet";t.href=r}document.getElementsByTagName("head")[0].insertBefore(t,document.getElementById("syntaxhighlighteranchor"))})();SyntaxHighlighter.config.strings.expandSource="+ expand source";SyntaxHighlighter.config.strings.help="?";SyntaxHighlighter.config.strings.alert="SyntaxHighlighter\n\n";SyntaxHighlighter.config.strings.noBrush="Can't find brush for: ";SyntaxHighlighter.config.strings.brushNotHtmlScript="Brush wasn't configured for html-script option: ";SyntaxHighlighter.defaults["class-name"]="code";SyntaxHighlighter.defaults["gutter"]=false;SyntaxHighlighter.defaults["pad-line-numbers"]=5;SyntaxHighlighter.defaults["toolbar"]=false;SyntaxHighlighter.all()
 	</script>
+
 	<?php } else {
+			echo '<br>';
+			echo '<div class="photo_index_wrap clearfix">';
+			echo '<div class="photo_index" style="position: relative; height: 100px;">';
 		// Lets list all albums
-		echo '<div id="flickrGal0">';
+		// echo '<div id="flickrGal0">';
 
-		// echo "<pre>";
-		// print_r($album);
-		// echo "</pre>";
-
+		$left = 0;
+		$top = 0;
+		$k = 0;
 		foreach ($album->photosets->photoset as $gallery):
-			echo '<a href="?g='.$gallery->attributes()->id.'"><h2>'.$gallery->title.'</h2></a>';
-		endforeach;
 
-		// echo "</div>";
+			// echo "<pre>";
+			// print_r($gallery);
+			// echo "</pre>";
+
+			// echo "<h1>".$gallery->title."</h1>";
+			// echo '<img src="https://farm'.$gallery->attributes()->farm.'.staticflickr.com/'.$gallery->attributes()->server.'/'.$gallery->attributes()->primary.'_'.$gallery->attributes()->secret.'_z.jpg">';
+				
+				$img = 'https://farm'.$gallery->attributes()->farm.'.staticflickr.com/'.$gallery->attributes()->server.'/'.$gallery->attributes()->primary.'_'.$gallery->attributes()->secret.'_z.jpg';
+				$name = $gallery->title;
+				$id = $gallery->attributes()->id;
+
+				$res = $k % 2;
+				if($res == 0 && $k > 0){
+					$top = $top + 343;	
+					$left = 0;
+				}
+				else{
+					if($k > 0){
+						$left = 510;
+					}
+				}
+
+				echo '<article style="position: absolute; left: '.$left.'px; top: '.$top.'px;">';
+					echo '<a href="?g='.$id.'" title="'.$name.'">';
+						echo '<figure>';
+							echo '<img class="lazy picz" data-original="'.$img.'" alt="'.$name.'" style="display: block;"/>';
+							echo '<span class="gallery_name"><span class="glyphicon glyphicon-camera"></span> '.$name.'</span>';
+						echo '</figure>';
+					echo '</a>';
+				echo '</article>';
+				$k++;
+
+		endforeach;
 
 	}
 ?>
-
-
-
+	</div>
+</div>
 
 </body>
 </html>
